bbca58f116f6fcf5803fec58b5a727b0f181019c
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-09-15 20:38:32 +0300

    created the main create table sql file

---
+++ create_tables.sql
@@ -0,0 +1,22 @@
+
+
+CREATE TABLE IF NOT EXISTS bands (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    hometown TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS venues (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    city TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS concerts (
+    id INTEGER PRIMARY KEY,
+    band_id INTEGER NOT NULL,
+    venue_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    FOREIGN KEY(band_id) REFERENCES bands(id),
+    FOREIGN KEY(venue_id) REFERENCES venues(id)
+);
